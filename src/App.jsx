7434356fa3dcb2c30b12245ff8f81f2506be67fd
PooyaLiebe <pooya.payvar26@gmail.com>
2025-01-11 07:20:38 +0000

    Modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import { FiSettings } from 'react-icons/fi'
+import { Tooltip } from '@mui/material'
+import { Navbar, Sidebar, ThemeSettings } from './components'
+import { Pie } from "./components"
+import {
+  Dashboard,
+  Forms,
+  OperatorSubmit,
+  Kanban,
+  Line,
+  Area,
+  Login,
+  Bar,
+  Start,
+  TechnicianLogin,
+  OperatorLogin,
+  SubmitForm,
+  Projects,
+} from "./pages";
+import "./App.css";
+import { useStateContext } from "./contexts/ContextProvider";
+import TechnicianSubmit from "./pages/TechnicianSubmit";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    <h1>Hello World</h1>
+      <h1>Hello World</h1>
     </>
   )
 }
