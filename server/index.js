bb525a6cd74356713ac1022c9b79bdb2f68e6d22
PooyaLiebe <pooya.payvar26@gmail.com>
2025-01-14 14:11:20 +0330

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@
 import express from "express";
 import connectDB from "./db.js";
 import mongoose from "mongoose";
-import itemModel from "./models/item.js";
-import FormModel from "./models/Login.js";
+import KarshenasModel from "./models/Karshenas.js";
+import OperatorModel from "./models/OperatorSubmit.js";
+import TechnicianModel from "./models/TechnicianSubmit.js";
 import cors from "cors";
-
+import AghlamModel from "./models/Aghlam.js";
 const app = express();
 
 const corsOptions = {
@@ -17,10 +18,11 @@ app.use(cors(corsOptions));
 app.use(express.json());
 connectDB();
 
+// Operatror DB
 app.post("/submitform", async (req, res) => {
   console.log(req.body);
   try {
-    const formInstance = new FormModel(req.body);
+    const formInstance = new OperatorModel(req.body);
     await formInstance.save();
     res
       .status(200)
@@ -44,20 +46,91 @@ app.post("/submitform", async (req, res) => {
   }
 });
 
-app.get("/linechart", async (req, res) => {
+// Technician DB
+app.post("/techniciansubmit", async (req, res) => {
+  console.log(req.body);
   try {
-    await mongoose.connect();
-    const database = mongoose.db("preventivewebapp");
-    const collection = database.collection("forms");
-    const data = await collection.find({ shift: { $exsists: true } }).toArray();
-    res.json(data);
+    const formInstance = new TechnicianModel(req.body);
+    await formInstance.save();
+    res
+      .status(200)
+      .json({ Status: "success", message: "Form data saved successfully!" });
+    console.log(formInstance);
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Server Error");
-  } finally {
-    await mongoose.close();
+    if (err.name === "ValidationError") {
+      res.status(400).json({
+        Status: "error",
+        Error: "Validation Error",
+        message: err.message,
+      });
+    } else {
+      res.status(500).json({
+        Status: "error",
+        Error: "Error saving form data.",
+        message: err.message,
+      });
+    }
+    console.error("Error saving form data:", err);
   }
 });
+
+// Aghlam DB
+app.post("/aghlam", async (req, res) => {
+  console.log(req.body);
+  try {
+    const formInstance = new AghlamModel(req.body);
+    await formInstance.save();
+    res
+      .status(200)
+      .json({ Status: "success", message: "Form data saved successfully!" });
+    console.log(formInstance);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res.status(400).json({
+        Status: "error",
+        Error: "Validation Error",
+        message: err.message,
+      });
+    } else {
+      res.status(500).json({
+        Status: "error",
+        Error: "Error saving form data.",
+        message: err.message,
+      });
+    }
+    console.error("Error saving form data:", err);
+  }
+});
+
+
+// Karshenas
+app.post("/technician", async (req, res) => {
+  console.log(req.body);
+  try {
+    const formInstance = new KarshenasModel(req.body);
+    await formInstance.save();
+    res
+      .status(200)
+      .json({ Status: "success", message: "Form data saved successfully!" });
+    console.log(formInstance);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res.status(400).json({
+        Status: "error",
+        Error: "Validation Error",
+        message: err.message,
+      });
+    } else {
+      res.status(500).json({
+        Status: "error",
+        Error: "Error saving form data.",
+        message: err.message,
+      });
+    }
+    console.error("Error saving form data:", err);
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server is Running");
 });
