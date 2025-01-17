555661e7507eeaeda3fec655ffc8f30965192367
PooyaLiebe <pooya.payvar26@gmail.com>
2025-01-17 14:56:25 +0330

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,13 @@
+export { default as Login } from "./Login";
 export { default as Dashboard } from "./Dashboard";
 export { default as SubmitForm } from "./SubmitForm";
-export { default as Kanban } from "./Kanban";
+export { default as HSE } from "./HSE";
 export { default as OperatorSubmit } from "./OperatorSubmit";
 export { default as OperatorLogin } from "./OperatorLogin";
 export { default as TechnicianSubmit } from "./TechnicianSubmit";
 export { default as TechnicianLogin } from "./TechnicianLogin";
 export { default as Technician } from "./Technician";
-export { default as Login } from "./Login";
+export { default as Kanban } from "./Kanban";
 export { default as Projects } from "./Projects";
 export { default as Start } from "./Start";
 export { default as Forms } from "./Forms";
