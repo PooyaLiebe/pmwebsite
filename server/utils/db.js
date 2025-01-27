eff81fc1f45d1bf3046e70c38be08381ffd92e8d
PooyaLiebe <pooya.payvar26@gmail.com>
2025-01-27 14:14:33 +0000

    Modify

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,11 +1,13 @@
+/* eslint-disable no-undef */
 import mysql from "mysql";
+import "dotenv/config";
 
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "planning_pooya",
-  password: "Po@60376911",
-  database: "planning_pm",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
 });
 
 con.connect(function (err) {
