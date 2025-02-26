0ab48d259d4b581451cc2eb5e9a172f669cc35cd
PooyaLiebe <pooya.payvar26@gmail.com>
2025-02-26 12:50:24 +0000

    create index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
+<<<<<<< HEAD
 
+=======
+import express from "express";
+import cors from "cors";
+import { AdminRouter } from "../Routes/PmRoutes.js";
+import { OperatorRouter } from "../Routes/OperatorRoutes.js";
+import { TechnicianRouter } from "../Routes/TechnicianRoutes.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/auth", AdminRouter);
+app.use("/operator", OperatorRouter);
+app.use("/technician", TechnicianRouter);
+
+app.listen(3000, () => {
+  console.log("Server is Running");
+});
+>>>>>>> 52a233e (create index.js)
